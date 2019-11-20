6913b40642553bd186edce9d4df7a198b202be0d
varshlav <46642344+varshlav@users.noreply.github.com>
2019-11-20 00:19:23 -0600

    Add files via upload

---
+++ action.js
@@ -0,0 +1,59 @@
+let arr = [];
+
+for (let i = 0; i < 90; i++) {
+    arr.push(i + 1);
+}
+
+
+let length = arr.length;
+let choosen;
+let displayDiv = document.getElementById('current');
+let message = document.getElementById('message');
+let rowName;
+let colName;
+
+
+
+function selectNumber() {
+    if (length > 0) {
+        rand = Math.floor(Math.random() * length);
+        choosen = arr.splice(rand, 1);
+        length--;
+        displayDiv.innerText = choosen;
+        colName = 'col' + choosen;
+        document.getElementById(colName).classList.add('strikeOff');
+    } else {
+        displayDiv.innerText = "**";
+        message.innerText = "There is no number available now !!"
+    }
+}
+
+
+
+
+
+
+
+
+
+let count = 1;
+let table = document.createElement('TABLE');
+table.setAttribute('id', 'myTable');
+document.getElementById('tableSpace').appendChild(table);
+for (let i = 0; i < 9; i++) {
+    rowName = 'row' + i;
+    let row = document.createElement('TR');
+    row.setAttribute('id', rowName);
+    document.getElementById('myTable').appendChild(row);
+    for (let j = 0; j < 10; j++) {
+        let col = document.createElement('TD');
+        colName = 'col' + count;
+        col.setAttribute('id', colName);
+        col.innerHTML = count;
+        count++;
+        document.getElementById(rowName).appendChild(col);
+    }
+
+
+}
+
