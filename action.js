b8a6147790fbb499aa6725b54c4f10be3ddacc5f
varshlav <46642344+varshlav@users.noreply.github.com>
2019-11-23 22:27:08 -0600

    Add files via upload

--- action.js
+++ action.js
@@ -11,17 +11,29 @@ let displayDiv = document.getElementById('current');
 let message = document.getElementById('message');
 let rowName;
 let colName;
+let flag = true;
 
 
 
 function selectNumber() {
+    if (!flag) {
+        return;
+    }
     if (length > 0) {
-        rand = Math.floor(Math.random() * length);
-        choosen = arr.splice(rand, 1);
-        length--;
-        displayDiv.innerText = choosen;
-        colName = 'col' + choosen;
-        document.getElementById(colName).classList.add('strikeOff');
+        flag = false;
+        displayDiv.classList.add('anim');
+        displayDiv.innerText = "";
+        setTimeout(function(){
+            rand = Math.floor(Math.random() * length);
+            choosen = arr.splice(rand, 1);
+            length--;
+            displayDiv.innerText = choosen;
+            colName = 'col' + choosen;
+            document.getElementById(colName).classList.add('strikeOff');
+            displayDiv.classList.remove('anim');
+            flag = true;
+        }, 2000);
+
     } else {
         displayDiv.innerText = "**";
         message.innerText = "There is no number available now !!"
